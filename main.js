62675eca816be14d5a165a9d6c14b26512ad7d59
Mateo Callemo <45512004+nixpath@users.noreply.github.com>
2018-12-02 12:59:51 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+// OpenFusky 2018 by Posixdeon, modified by Nixpath.
+var http = require("http");
+var fs = require("fs");
+// This is dictionary
+var dicto = require("./dicto");
 
+fs.readFile('./index.html', function (err, html) {
+
+    if (err) throw err;    
+
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(3000);
+});
+
+console.log("Access to bot is on localhost:3000 (type this in your browser).");
